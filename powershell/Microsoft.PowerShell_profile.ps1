0b5285c3ff33167266bc3ca427b15900d01f9f45
Careca Voador <e.rodrigo@outlook.com>
2022-11-08 13:34:46 -0300

    PS Profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,31 @@
-C:/Users/Everton Souza/OneDrive/Clicheria/Documentos/PowerShell/Microsoft.PowerShell_profile.ps1
+# Variáveis de ambiente
+$venvs = 'C:\Users\Everton Souza\.virtualenvs'
+
+
+# Aliases
+Set-Alias v nvim
+
+# FUNÇÕES
+# Contabiliza o tempo que um comando levou para executar
+function time {
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]$comando
+    )
+    If ($comando) {
+        $tempo = Measure-Command {Invoke-Expression $comando} | Select-Object -ExpandProperty TotalMilliseconds
+        Write-Host "Levou $($tempo) milisegundos para executar."
+    } else {
+        Write-Host "É preciso especificar um comando."
+        Write-Host "`tEXEMPLO: time {python main.py}"
+    }
+}
+
+
+# Inicializa Oh My Posh
+$tema = 'C:\Users\Everton Souza\.config\powershell\themes\meutema.omp.toml'
+oh-my-posh init pwsh --config $tema | Invoke-Expression
+
+
+# Terminal Icons
+Import-Module -Name Terminal-Icons
